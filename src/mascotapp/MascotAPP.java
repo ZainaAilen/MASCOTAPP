cf714670a807f8f88eda8f776c25c5999aae63b6
AilenZaina <ailenzaina96@gmail.com>
2022-05-04 07:19:30 -0300

    MASCOTAPP MASCOTAP

---
+++ src/mascotapp/MascotAPP.java
@@ -0,0 +1,19 @@
+package mascotapp;
+ 
+import mascotapp.entidades.Mascota;
+import mascotapp.servicios.ServicioMascota;
+
+public class MascotAPP {
+
+    public static void main(String[] args) {
+        
+        ServicioMascota sm = new ServicioMascota();
+
+        Mascota m1 = sm.crearMascota();
+        
+        System.out.println(m1);
+
+        m1 = null;
+        
+    }
+}
